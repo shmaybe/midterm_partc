e68cc5be9719f7ba7881706ddf941e28db7c7065
May Tran <tranmay@sheridancollege.ca>
2023-03-08 17:05:44 -0500

    add comment for may

---
+++ src/midtermexam_versiona_extensioncode/may.java
@@ -0,0 +1,14 @@
+package midtermexam_versiona_extensioncode;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MayxT
+ */
+public class may {
+    //this is a comment
+}
